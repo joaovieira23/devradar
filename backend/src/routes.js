64a00638af11755491a854319e0bf0620674a154
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-04-25 18:46:02 -0300

    chore: consume github api

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,15 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
+routes.post('/devs', async (req, res) => {
+  const { github_username } = req.body;
+
+  const response = await axios.get(`https://api.github.com/users/${github_username}`);
+
+  console.log(response.data);
+
   return res.json({ message: 'Hello Vieira' });
 });
 
