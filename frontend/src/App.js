e23509d03b2fe8e7ca97d926543d3c13b01ea7a0
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-04-26 18:51:00 -0300

    chore: define initial frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
