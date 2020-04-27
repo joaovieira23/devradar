ef77556295cc7adcbc095ff4272d18ff34777d40
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-04-26 21:16:13 -0300

    chore: add global and sidebar styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
+import './global.css'
+import './App.css'
+import './Sidebar.css'
+
 function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
 
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+
+          <button type="submit">Salvar</button>
+        </form>
       </aside>
       <main>
 
